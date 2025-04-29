29f47a07aea5a4609624d41276f686927915e1e7
Toni Ramírez <toni@polygon.technology>
2025-04-29 09:49:40 +0200

    feat: test

--- aggregator/aggregator_test.go
+++ aggregator/aggregator_test.go
@@ -84,7 +84,7 @@ func Test_Start(t *testing.T) {
 	mockEthTxManager := new(mocks.EthTxManagerClientMock)
 
 	mockL1Syncr.On("Sync", mock.Anything).Return(nil)
-	mockEtherman.On("GetLatestVerifiedBatchNum").Return(uint64(90), nil).Once()
+	mockEtherman.On("GetLatestVerifiedBatchNum").Return(uint64(90), nil)
 	mockEtherman.On("GetBatchAccInputHash", mock.Anything, uint64(90)).Return(common.Hash{}, nil).Once()
 	mockStorage.On("DeleteGeneratedProofs", mock.Anything, uint64(90), mock.Anything, nil).Return(nil).Once()
 	mockStorage.On("CleanupLockedProofs", mock.Anything, "", nil).Return(int64(0), nil)
